03880023f2aa4f0bfcf88bb52d22243159e09796
Boris <jesteracer@me.com>
2020-02-28 13:24:12 +0200

    Update FlutterWebView.m

--- packages/webview_flutter/ios/Classes/FlutterWebView.m
+++ packages/webview_flutter/ios/Classes/FlutterWebView.m
@@ -91,6 +91,7 @@
     [self updateAutoMediaPlaybackPolicy:args[@"autoMediaPlaybackPolicy"]
                         inConfiguration:configuration];
 
+    configuration.allowsInlineMediaPlayback = true;
     _webView = [[FLTWKWebView alloc] initWithFrame:frame configuration:configuration];
     _navigationDelegate = [[FLTWKNavigationDelegate alloc] initWithChannel:_channel];
     _webView.UIDelegate = self;
